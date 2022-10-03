07da7688e70ca05667ccfee2e8a240d4094b055a
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-10-03 00:03:00 -0400

    Updated Favorite SF symbol view

--- ButtonPallete/Button Views/Favorite/FavoriteView.swift
+++ ButtonPallete/Button Views/Favorite/FavoriteView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct FavoriteView: View {
+
+    @State private var userTapped: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(systemName: "heart.fill")
+                .opacity(userTapped ? 1 : 0)
+                .scaleEffect(userTapped ? 1.0 : 0.1)
+                .animation(.linear)
+            Image(systemName: "heart")
+        }
+        .font(.system(size: 40))
+            .onTapGesture {
+                self.userTapped.toggle()
+            }
+        .foregroundColor(userTapped ? .red : .tomatoeRed)
     }
 }
 
---
+++ ButtonPallete/Extensions/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  ButtonPallete
+//
+//  Created by Neftali Samarey on 10/2/22.
+//
+
+import SwiftUI
+
+extension Color {
+    // Cream UI white - rgb(247, 241, 227)
+    static let creamUI = Color(red: 247/255, green: 241/255, blue: 227/255)
+
+    // Romatoe Red UI red - rgb(255, 82, 82)
+    static let tomatoeRed = Color(red: 255/255, green: 82/255, blue: 82/255)
+}
